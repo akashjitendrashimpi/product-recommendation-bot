4620d1fe449bc122390fd6cafe3ac00248256f33
Akash <akashjshimpi2008@gmail.com>
2026-02-06 12:32:01 +0530

    Add Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next"
 import type React from "react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
@@ -20,6 +21,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={`font-sans antialiased`}>
         {children}
+        <Analytics />
+
       </body>
     </html>
   )
